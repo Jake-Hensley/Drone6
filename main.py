0e255fdd4deadc5086ef6ddee0cc9d2ebdf31b62
Jake Hensley <hensley.j@northeastern.edu>
2024-10-15 19:43:39 -0400

    added PIDController class and the gyroscope class, added a little processing to main

--- main.py
+++ main.py
@@ -1,54 +1,19 @@
+import MPU6050
 from machine import Pin
 from utime import sleep
 import machine
 
+"""creates an I2C object with the id of 0, with data on pin 12, and clock on pin 13"""
 i2c = machine.I2C(0, sda=machine.Pin(12), scl=machine.Pin(13))
 
 imu = MPU6050(i2c)
 
 imu.wake()
 
+"""Tuple type of (x, y, z)"""
 gyro_data = imu.read_gyro_data()
 
 print(gyro_data)
-
-class PIDController:
-
-    def __init__(self, kp:float, ki:float, kd:float, cycle_time_seconds:float, i_limit:float = 10.0) -> None:
-        # settings
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
-        self.cycle_time_seconds = cycle_time_seconds
-        self.i_limit = i_limit
-
-        # state variables
-        self.previous_error:float = 0.0
-        self.previous_i:float = 0.0
-
-    def calculate(self, actual:float, goal:float) -> float:
-        error = goal - actual
-
-        # P term
-        P:float = error * self.kp
-
-        # I term
-        I:float = self.previous_i + (error * self.ki * self.cycle_time_seconds)
-        I = max(min(I, self.i_limit), self.i_limit * -1)
-
-        # D term
-        D:float = self.kd * (error - self.previous_error) / self.cycle_time_seconds
-
-        # set state variables for next time
-        self.previous_error = error
-        self.previous_i = I
-
-        # return
-        return P + I + D
-    
-    def reset(self) -> None:
-        self.previous_error = 0.0
-        self.previous_i = 0.0
 
 t1:float = pid_pitch + pid_roll - pid_yaw
 t2:float = pid_pitch - pid_roll + pid_yaw
